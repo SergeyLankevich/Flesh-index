1728a01e48c5403a5963d60eeeb6696b68ed7493
MalakhovIlyaPavlovich <malakhov.ilya.pavlovich@yandex.ru>
2019-11-17 13:48:03 +0700

    We need to do tonality for russian text, output and localization.

--- functions_en_ru.py
+++ functions_en_ru.py
@@ -1,56 +1,77 @@
 import textblob as tb
 
 
-def func_en(text):
-    # Количество слогов
+def lang_inp():
+    language = input('Choose language:\n1. English\n2. Russian\n')
+    while True:
+        if (language.lower() == 'english' or language.lower() == 'en' or language == '1' or language == '1.' or
+                language.lower() == '1. english'):
+            language = 1
+            break
+        elif (language.lower() == 'russian' or language.lower() == 'ru' or language == '2' or language == '2.' or
+                language.lower() == '2. russian'):
+            language = 2
+            break
+        else:
+            language = input('Choose language from the proposed:\n1. English\n2. Russian\n')
+    return language
+
+
+def func_en(text, lang):
+    if lang == 1:
+        import local_en as loc
+    elif lang == 2:
+        import local_ru as loc
+
+    # Syllable count
     vowels_en = list('aeiouy')
-    syllables_count = 1 if text[0] in vowels_en else 0
+    syllable_count = 1 if text[0] in vowels_en else 0
     for i in range(1, len(text)):
         if (text[i].lower() in vowels_en or
                 (text[i] in 'lmn' and text[i + 1].lower() not in vowels_en and text[i - 1].lower() not in vowels_en)):
-            syllables_count += 1
+            syllable_count += 1
 
-    # Количество предложений
-    sentences_count = 0
+    # Sentence count
+    sentence_count = 0
     for i in range(len(text) - 1):
         if text[i + 1] in '.!?' and text[i] not in '.!?':
-            sentences_count += 1
+            sentence_count += 1
 
-    # Количество слов
+    # Word count
     words = text.split()
-    words_count = len(words)
+    word_count = len(words)
 
-    # Индекс Флеша
-    ASL = words_count / sentences_count
-    ASW = syllables_count / words_count
+    # Flesh index
+    ASL = word_count / sentence_count
+    ASW = syllable_count / word_count
     FRE = 206.835 - 1.015 * ASL - 84.6 * ASW
 
-    # Сложность текста
+    # Text complexity
     if FRE >= 90:
-        complexity = 'Очень легко читается.'
+        complexity = loc.COMPLEXITY_VERY_EASY
     elif FRE >= 65:
-        complexity = 'Легко читается.'
+        complexity = loc.COMPLEXITY_EASY
     elif FRE >= 30:
-        complexity = 'Немного трудно читать.'
+        complexity = loc.COMPLEXITY_HARD
     else:
-        complexity = 'Очень трудно читать.'
+        complexity = loc.COMPLEXITY_VERY_HARD
 
-    # Объекстивность текста
+    # Text objectivity
     b = tb.TextBlob(text)
     objectivity = str(((1 - b.sentiment.subjectivity)*100)) + '%'
 
-    # Тональность текста
+    # Tonality of the text
     if b.sentiment.polarity >= 0.5:
-        tonality = 'Позитивный'
+        tonality = loc.POLATITY_POZITIVE
     elif b.sentiment.polarity > -0.5:
-        tonality = 'Нейтральный'
+        tonality = loc.POLARITY_NEUTRAL
     else:
-        tonality = 'Негативный'
+        tonality = loc.POLARITY_NEGATIVE
 
-    # Ответ
-    response = {'sentences_count': sentences_count,
-                'words_count': words_count,
-                'syllables_count': syllables_count,
+    # Response
+    response = {'sentence_count': sentence_count,
+                'word_count': word_count,
+                'syllable_count': syllable_count,
                 'FRE': FRE,
                 'ASL': ASL,
                 'ASW': ASW,
@@ -61,52 +82,58 @@ def func_en(text):
     return response
 
 
-def func_ru(text):
-    # Количество слогов
+def func_ru(text, lang):
+    if lang == 1:
+        import local_en as loc
+    elif lang == 2:
+        import local_ru as loc
+
+    # Syllable count
     vowels_ru = list('аеёиоуыэюя')
-    syllables_count = 0
+    syllable_count = 0
     for c in text:
         if c.lower() in vowels_ru:
-            syllables_count += 1
+            syllable_count += 1
 
-    # Количество предложений
-    sentences_count = 0
+    # Sentence count
+    sentence_count = 0
     for i in range(len(text) - 1):
         if text[i + 1] in '.!?' and text[i] not in '.!?':
-            sentences_count += 1
+            sentence_count += 1
 
-    # Количество слов
+    # Word count
     words = text.split()
-    words_count = len(words)
+    word_count = len(words)
 
-    # Индекс Флеша
-    ASL = words_count / sentences_count
-    ASW = syllables_count / words_count
+    # Flesh index
+    ASL = word_count / sentence_count
+    ASW = syllable_count / word_count
     FRE = 206.835 - 1.3 * ASL - 60.1 * ASW
 
-    # Сложность текста
+    # Text complexity
     if FRE >= 90:
-        complexity = 'Очень легко читается.'
+        complexity = loc.COMPLEXITY_VERY_EASY
     elif FRE >= 65:
-        complexity = 'Легко читается.'
+        complexity = loc.COMPLEXITY_EASY
     elif FRE >= 30:
-        complexity = 'Немного трудно читать.'
+        complexity = loc.COMPLEXITY_HARD
     else:
-        complexity = 'Очень трудно читать.'
+        complexity = loc.COMPLEXITY_VERY_HARD
 
-    # Тональность текста
+    # Tonality of the text
+    # TODO
     x = True
     if x:
-        tonality = 'Позитивный'
+        tonality = loc.POLATITY_POZITIVE
     elif x:
-        tonality = 'Нейтральный'
+        tonality = loc.POLARITY_NEUTRAL
     else:
-        tonality = 'Негативный'
+        tonality = loc.POLARITY_NEGATIVE
 
-    # Ответ
-    response = {'sentences_count': sentences_count,
-                'words_count': words_count,
-                'syllables_count': syllables_count,
+    # Response
+    response = {'sentence_count': sentence_count,
+                'word_count': word_count,
+                'syllable_count': syllable_count,
                 'FRE': FRE,
                 'ASL': ASL,
                 'ASW': ASW,
--- ilya.py
+++ ilya.py
@@ -1,7 +1,12 @@
 from functions_en_ru import *
 
+lang = lang_inp()
+if lang == 1:
+    import local_en as loc
+elif lang == 2:
+    import local_ru as loc
 
-text = input('Введите текст: ')
+text = input(loc.TEXT_INPUT)
 language = ''
 # Корректность ввода
 while True:
@@ -18,16 +23,15 @@ while True:
     else:
         language = 'english' if english else 'russian'
         break
-    text = input('Введите текст на одном языке: ')
+    text = input(loc.TEXT_LANGUAGE)
 
 # Точка в конце текста
 if text[-1] not in list('.!?'):
     text = text + '.'
 
 if language == 'english':
-    response = func_en(text)
+    response = func_en(text, lang)
     print(response)
-
     '''
     Предложений:
     Слов:
@@ -40,7 +44,7 @@ if language == 'english':
     Объективность:
     '''
 else:
-    response = func_ru(text)
+    response = func_ru(text, lang)
     print(response)
     '''
     Предложений:
